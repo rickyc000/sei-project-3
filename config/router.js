6caeea06aae5d4f4e75118cfbf038c27511cc0b4
Elsie Down <elspethdown@gmail.com>
2021-01-07 21:16:59 +0000

    added requests for spaces controllers

--- config/router.js
+++ config/router.js
@@ -5,5 +5,11 @@ const router = express.Router()
 
 router.route('/spaces')
   .get(spaces.index)
+  .post(spaces.create)
+
+router.route('/spaces/:id')
+  .get(spaces.show)
+  .put(spaces.update)
+  .delete(spaces.delete)
 
 export default router
--- controllers/spaces.js
+++ controllers/spaces.js
@@ -1,6 +1,7 @@
 import Space from '../models/space.js'
 
 // spaces index
+
 async function spaceIndex(_req, res, next){
   try {
     const spaces = await Space.find()
@@ -10,6 +11,64 @@ async function spaceIndex(_req, res, next){
   }
 }
 
+// spaces create 
+
+async function spaceCreate (req, res, next){
+  try {
+    const newSpace = await Space.create(req.body)
+    return res.status(201).json(newSpace)
+  } catch (err) {
+    next(err)
+  }
+}
+
+// spaces show
+
+async function spaceShow(req, res, next){
+  const { id } = req.params
+  try {
+    const space = await Space.findById(id)
+    if (!space) throw new Error()
+    return res.status(200).json(space)
+  } catch (err) {
+    next(err)
+  }
+}
+
+// space update / edit
+
+async function spaceUpdate(req, res, next){
+  const { id } = req.params
+  try {
+    const spaceToEdit = await Space.findById(id)
+    if (!spaceToEdit) throw new Error()
+    Object.assign(spaceToEdit, req.body)
+    await spaceToEdit.save()
+    return res.status(202).json(spaceToEdit)
+  } catch (err) {
+    next(err)
+  }
+}
+
+// space delete
+
+async function spaceDelete(req, res, next) {
+  const { id } = req.params
+  try {
+    const spaceToDelete = await Space.findById(id)
+    if (!spaceToDelete) throw new Error()
+    await spaceToDelete.remove()
+    return res.sendStatus(204)
+  } catch (err) {
+    next(err)
+  }
+}
+
+
 export default {
   index: spaceIndex,
+  create: spaceCreate,
+  show: spaceShow,
+  update: spaceUpdate,
+  delete: spaceDelete,
 }
