e659322c3a213010c0185fdf2411ab14b5015d66
Elsie Down <elspethdown@gmail.com>
2021-01-08 10:45:08 +0000

    update longlat for seed locations

--- models/space.js
+++ models/space.js
@@ -4,6 +4,7 @@ const citySpaceSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true, maxlength: 400 },
   image: { type: String, required: true },
+  location: { type: Object, required: true },
   // owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true  },
 })
 
