4f27e46a3548ef2bfa763c5a8ffe056109f29af4
Edwyn <edwyn.3000@btinternet.com>
2021-01-08 10:05:03 +0000

    additional spaces

--- db/data/spaces.js
+++ db/data/spaces.js
@@ -3,5 +3,60 @@ export default [
     name: 'Victoria Park',
     description: 'Victoria Park (known colloquially as Vicky Park or the People Park) is a park in the London Borough of Tower Hamlets in East London, England. It is the largest park in Tower Hamlets and one of London most visited green spaces with approximately 9 million visitors every year. The park spans 86.18 hectares (213.0 acres) of open space and opened to the public in 1845.',
     image: 'https://london-travel.co.uk/static/b5fd2d5ae890131e1d102af007f3db2d/14b42/victoria-park-japanese-pagoda.jpg',
+  },
+{
+    name: 'Bushy Park',
+    description: 'Bushy Park, which is a short distance from Hampton Court station or a bus from Kingston, is far wilder, has way more deer, and theres a mile-long tree-lined avenue designed by Christopher Wren, punctuated by a massive eighteenth-century statue of the Roman goddess Diana. The water garden is possibly the best spot for solitude, tucked away from the crowds, where the only noise youll hear are ducks flapping their wings in the spray',
+    image: 'https://media.timeout.com/images/103235416/750/422/image.jpg',
+  },
+{
+    name: 'Wat Buddhapadipa',
+    description: 'The first temple of its kind to be built in the UK, this red-roofed haven is moments away from Wimbledon Common. It’s a Thai Buddhist temple but anyone is welcome to visit the temple or walk around in the grounds, and the sanctuary garden is a top spot for quiet. No one speaks in a raised voice around here, and there are little signs with positive thoughts and affirmations. They also hold regular meditation classes and silent retreats.',
+    image: 'https://media.timeout.com/images/105620913/750/422/image.jpg',
+  },
+{
+    name: 'Phytology Medicinal Garden',
+    description: 'This artist-led project has transformed a WWII bombsite into an impressive garden that provides free food for local communities. We often consider the plants they grow to be weeds, but they’ve been picked for their historic and modern-day use in phytotherapy. That’s the use of plant extracts as medicines or health-promoting agents, FYI. You can come down to the physic garden and pick some of 30-plus varieties of plants – with the supervision of staff – but they also have a deep investment in finding out how city living impacts mental health. The underlying ethos is that they provide a space that acts as an antidote to some of London’s stresses.',
+    image: 'https://media.timeout.com/images/105291491/750/422/image.jpg',
+  },
+{
+    name: 'Word on the Water',
+    description: 'Book-related places breathe out calm, thoughtful vibes by their nature. While the British Library and the reading room at the Wellcome Trust remain firm, if obvious, favourites, a rising contender is Word on the Water, the 1920s Dutch barge that has been converted into a floating bookshop. Located on Granary Square (arguably a quiet spot unto itself as long as you stick to the canal away from the water fountains), it offers a polar opposite experience to the sprawl and stress around the station.',
+    image: 'https://media.timeout.com/images/103852801/750/422/image.jpg',
+  },
+{
+    name: 'A butterfly house at Clissold Park',
+    description: 'Next to the deer and goats that graze in Stoke Newington’s small ‘zoo’ is a sanctuary for our most beautiful insect family. Native and tropical butterflies flit between flowers and plants in the warm glass dome, which is open for periodic free tours from May to September – just watch where you’re treading!',
+    image: 'https://media.timeout.com/images/102760625/750/422/image.jpg',
+  },
+{
+    name: 'Diana Princess of Wales Memorial walk',
+    description: 'Ninety plaques in the ground mark this memorial walk, which covers four parks, three palaces, two mansions and lots of meaningful locations associated with the Princess. Check off four of eight royal parks as you wander through *St James’s Park*, *Green Park*, *Hyde Park* and *Kensington Gardens*. If you’ve got restless little ones on your hands, stop at the *Diana Princess of Wales Memorial Playground*. Kids love the pirate ship, teepees and sculptures, all inspired by the magical adventures of Peter Pan. The route runs in a sweeping figure of eight, so join the walk anywhere along the way and end up back where you began – *Hyde Park Corner* is roughly at the midpoint making it a good starting point to pick up either side of the loop. ',
+    image: 'https://media.timeout.com/images/105362821/750/422/image.jpg',
+  }
+{
+    name: 'St Dunstan-in-the-East',
+    description: 'For such a small spot, St Dunstan-in-the-East has been through a lot; the twelfth-century church was almost wrecked in the Great Fire of London of 1666, but Sir Christopher Wren added a tower and steeple in an attempt to patch it up. It was damaged again in the Blitz of 1941, but Wren’s creation remains standing and now nature is reclaiming the ruins granting it a beautiful half-forgotten, peaceful atmosphere, with climbers running up entire walls. Although it’s right near a Premier Inn, its location in the back streets means most people don’t know it’s there. At any time, it promises to be a pretty spot to stop and think. ',
+    image: 'https://media.timeout.com/images/103936367/750/422/image.jpg',
+  },
+{
+    name: 'Epping Forest',
+    description: 'The extensive forest is a an area of conservation, and houses grassland, wetland, and thousands of rare plants and insects. Wedged in between the outskirts of East London and Sussex, Epping Forest was once a royal hunting ground. Nowadays you’re more likely to see families out for leisurely strolls, or dogs chasing squirrels through the bushes, but it’s a place brimming with natural beauty. Epping Forest is a one of the best quiet places in London to spend some hours walking, cycling, or relaxing in the sun. Taking a walk is a great way to clear your head, and with its numerous windy trails Epping Forest is the ideal place to take your mind of the stresses of city life and reconnect with the natural world that often seems like a foreign planet to London.',
+    image: 'https://www.justapack.com/wp-content/uploads/2019/04/quiet-places-in-london-epping-forest-1100x734.jpg',
+  },
+{
+    name: 'The Nomadic Community Gardens in Shoreditch',
+    description: 'The Nomadic Community Gardens is one of the most interesting of our quiet places in London selection, and one of the city’s most innovative spaces dedicated to helping the community. Their ethos is fascinating, and the atmosphere of the place is something truly remarkable. This non-profit organization based in Shoreditch aims to create a space for growing, learning, and sharing in order to combat the alienation and isolation fostered by city life. It’s also a great spot to visit and relax in London!',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Nomadic_Gardens.jpg/1024px-Nomadic_Gardens.jpg',
+  },
+{
+    name: 'Sail away on the Serpentine',
+    description: 'Take a boat trip in the Serpentine. Choose between rowing or pedalloing into the centre of Hyde Park’s peaceful 40-acre haven of still water, stop your chosen vessel and just… drift. If powering your own launch seems too much like hard work for a truly lazy day, you could always opt for a trip on the UK’s first Solarshuttle, a magnificent machine which glides silently across London’s most beautiful central lake, powered only by the sun. You can’t go wrong with a wood-fired pizza from the Serpentine Bar & Kitchen!',
+    image: 'https://assets.londonist.com/uploads/2017/05/i730/w_992.jpg',
+  },
+{
+    name: 'Explore a watery world at London Wetlands Centre',
+    description: 'Spend a chilled day wandering around the London Wetland Centre in Barnes, exploring 100 acres of lakes, ponds and gardens, spotting birds and pretending you’re deep in the English countryside, despite being firmly inside zone 3. If you’re after a really quiet day out, you might want to check there aren’t any special events planned; the London Wetland Centre hosts family-friendly activities throughout the year, but sometimes you might want to dodge the crowds. If you’re after a bit of excitement, make sure you see the otters being fed.',
+    image: 'https://assets.londonist.com/uploads/2017/05/i730/13947568455_a3ff4ba465_b.jpg',
   }
 ]
