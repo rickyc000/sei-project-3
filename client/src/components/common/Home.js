c7c8cdbad5ad97241b00082eaee0429a5c808f89
tobil <tobilesi@gmail.com>
2021-01-08 00:39:25 +0000

    semantic front end template for home page and nav bar

--- client/src/components/common/Home.js
+++ client/src/components/common/Home.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  Button,
+  Container,
+  Header
+} from 'semantic-ui-react'
 
 function Home() {
 
   return (
-    <h1>Home</h1>
+    <Container text className="container">
+      <div className="header">
+        <h1>
+          cityspace
+        </h1>
+      </div>
+
+      <Header
+        as='h2'
+        content='Discover and share your favourite spots in the city.'
+        className="title"
+      />
+      <div className="button-wrapper">
+        <Link to="/spaces" className="navbar-item">
+          <Button primary size='huge' animated='fade'>
+            <Button.Content visible>Find your happy place</Button.Content>
+            <Button.Content hidden>London is waiting</Button.Content>
+          </Button>
+        </Link>
+      </div>
+    </Container>
   )
 }
 
--- client/src/components/common/Nav.js
+++ client/src/components/common/Nav.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  Button,
+  Container,
+  Menu
+} from 'semantic-ui-react'
+
 
 function Nav() {
 
   return (
-    <h1>Nav</h1>
+    <Menu
+      size='large'
+      pointing secondary
+    >
+      <Container>
+        <Link to="/" className="navbar-item">
+          <Menu.Item
+            className="home"
+            name='home'
+          />
+        </Link>
+        <Menu.Item position='right'>
+          <Button as='a' >
+            Log in
+          </Button>
+          <Button as='a' style={{ marginLeft: '0.5em' }}>
+            Sign Up
+          </Button>
+        </Menu.Item>
+      </Container>
+    </Menu>
   )
 }
 
