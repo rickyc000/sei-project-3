759831a42f7d6af6b7d4f1c49adca8e2b5c8be5b
Elsie Down <elspethdown@gmail.com>
2021-01-12 10:26:52 +0000

    fixed route issue

--- client/src/components/spaces/indexPage/SpaceIndexCategories.js
+++ client/src/components/spaces/indexPage/SpaceIndexCategories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
-import { getAllSpaces } from '../lib/api'
+import { getAllSpaces } from '../../lib/api'
 
 
 function SpaceIndexCategories() {
