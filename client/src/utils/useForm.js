e291d064074a62bad6d9130021d3148b372179c7
tobil <tobilesi@gmail.com>
2021-01-13 19:59:59 +0000

    sign up register has the tags to select from whoop whoop

--- client/src/utils/useForm.js
+++ client/src/utils/useForm.js
@@ -7,7 +7,8 @@ function useForm(initialState) {
   const [formdata, setFormdata] = React.useState(initialState)
   const [errors, setErrors] = React.useState(initialState)
   const handleChange = event => {
-    const nextState = { ...formdata, [event.target.name]: event.target.value }
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+    const nextState = { ...formdata, [event.target.name]: value }
     const nextErrorState = { ...errors, [event.target.name]: '' }
     setFormdata(nextState)
     setErrors(nextErrorState)
