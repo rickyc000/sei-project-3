0eebb586d9301d3317d864b801365ad806cef2c3
rickyc <r.cato@live.com>
2021-01-13 11:11:26 +0000

    create form with jacks changes

--- client/src/components/spaces/SpaceForm.js
+++ client/src/components/spaces/SpaceForm.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function SpaceForm({ formdata, handleChange, handleSubmit, buttonText = 'Submit' }) {
-  
+
   return (
     <section className="form-container">
       <div className="form-box">
         <form className="ui form" onSubmit={handleSubmit}>
-    
+
           <div className="field">
             <label className="label">Name</label>
             <div className="control">
@@ -17,7 +17,7 @@ function SpaceForm({ formdata, handleChange, handleSubmit, buttonText = 'Submit'
                 value={formdata.name}
               />
             </div>
-          
+
           </div>
           <div className="field">
             <label className="label">Description</label>
@@ -29,7 +29,7 @@ function SpaceForm({ formdata, handleChange, handleSubmit, buttonText = 'Submit'
                 value={formdata.description}
               />
             </div>
-        
+
           </div>
           <div className="field">
             <label className="label">Image URL</label>
@@ -41,7 +41,7 @@ function SpaceForm({ formdata, handleChange, handleSubmit, buttonText = 'Submit'
                 value={formdata.image}
               />
             </div>
-          
+
           </div>
           <div className="field">
             <label className="label">Postcode</label>
@@ -53,9 +53,13 @@ function SpaceForm({ formdata, handleChange, handleSubmit, buttonText = 'Submit'
                 value={formdata.location}
               />
             </div>
-          
+            <div className="ui label">
+
+              Verify Postcode
+            </div>
+
           </div>
-        
+
 
           <div className="field">
             <button type="submit" className="ui button">{buttonText}</button>
