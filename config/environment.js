09a4784ff211236d29aa9cf89475fbbcd08321fb
rickyc <r.cato@live.com>
2021-02-11 15:24:35 +0000

    deployment prep

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,10 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/cityspace-db'
-export const secret = 'hello this is my secret'
+// export const port = 4000
+// export const dbURI = 'mongodb://localhost/cityspace-db'
+// export const secret = 'hello this is my secret'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/cityspace-db'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'
--- index.js
+++ index.js
@@ -4,16 +4,20 @@ import connectToDatabase from './lib/connectToDb.js'
 import router from './config/router.js'
 import logger from './lib/logger.js'
 import errorHandler from './lib/errorHandler.js'
+import path from 'path'
 
 const app = express()
+const __dirname = path.resolve()
 
 async function startServer() {
   try {
     await connectToDatabase()
     console.log('🤖 Database has connected')
+    app.use(express.static(`${__dirname}/client/build`))
     app.use(express.json())
     app.use(logger)
     app.use('/api', router)
+    app.use('/*', (_, res) => res.sendFile(`${__dirname}/client/build/index.html`))
     app.use(errorHandler)
     app.listen(port, () => console.log(`🤖 Up and running on port ${port}`))
   } catch (err) {
